4befe0a622baf97db3c3e13ec9b3cecde70fffef
Zeeee <129929654+Luxess24@users.noreply.github.com>
2025-10-18 13:55:18 +0800

    Lines Deletion
    Deleted duplicate lines in server.ts

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ import scheduleRoutes from "./Routes/scheduleRoutes.js";
 import adminRoutes from './Routes/adminRoutes.js';
 import rewardRoutes from "./Routes/rewardRoutes.js";
 import profileRoutes from './Routes/profileRoutes.js';
-import adminRoutes from './Routes/adminRoutes.js';
+
 // Load environment variables
 dotenv.config();
 
